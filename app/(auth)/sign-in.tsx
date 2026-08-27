96146080ded975b99d5ba27577670be66676f42b
Kelsey Paige Smith <pages.kelsey@gmail.com>
2026-08-27 00:42:34 -0400

    Added SafeAreaView for auth

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,13 +1,17 @@
 import { Link } from "expo-router";
+import { styled } from "nativewind";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 const SignIn = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-white p-5">
       <Text>Sign In Screen Placeholder</Text>
       <Link href="/(auth)/sign-up">{"Don't have an account? Sign up"}</Link>
-    </View>
+    </SafeAreaView>
   );
 };
 
